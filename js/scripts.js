8bf6115b335c19a6057d811ed4d4847d9cdd9a5e
byvime <149428592+byvime@users.noreply.github.com>
2023-11-30 18:31:55 -0800

    updates hamburguer icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 // script.js
-console.log("Hi! Welcome to My Portfolio Site");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
